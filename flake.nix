83c265af040d0189489168708168d94c2970ba1b
thrombe <yash.n.thombare620@gmail.com>
2025-11-04 01:56:19 +0530

    update hyprland to 1.51.1

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     hyprland = {
-      url = "github:hyprwm/Hyprland/v0.51.0";
+      url = "github:hyprwm/Hyprland/v0.51.1";
     };
     nixpkgs.follows = "hyprland/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
